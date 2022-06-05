4de7db10430e54a9ee6c28a70ffee8f61effb361
seulali <seulali79@gmail.com>
2022-06-05 21:52:57 +0900

    방 상세 화면에 데이터를 받아와 반영하는 작업

--- app/src/main/java/com/example/landapp_molly/ViewRoomDetailActivity.kt
+++ app/src/main/java/com/example/landapp_molly/ViewRoomDetailActivity.kt
@@ -2,10 +2,22 @@ package com.example.landapp_molly
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.landapp_molly.datas.Room
+import kotlinx.android.synthetic.main.activity_view_room_detail.*
 
 class ViewRoomDetailActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_room_detail)
+
+        val roomData = intent.getSerializableExtra("room") as Room // Roomdata 가져 옴
+
+        priceTxt.text = roomData.getFormattedPrice()
+        descriptionTxt.text = roomData.description
+        addressTxt.text = roomData.address
+        floorTxt.text = roomData.getFormattedFloor()
+
+
     }
 }
